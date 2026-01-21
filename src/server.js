eda8a3160cab8d3fa28137373d9831bd4aff1a11
SAIxANIRUDH <chouratsai007@gmail.com>
2026-01-21 09:03:59 +0530

    third commit

--- src/server.js
+++ src/server.js
@@ -4,6 +4,6 @@ const connectDB = require('./db/db');
 
 connectDB();
 
-app.listen(5000, () => {
-  console.log('Server is running on port 5000 ');
+app.get('/', (req, res) => {
+  res.send('Server is running on port 5000');
 });
