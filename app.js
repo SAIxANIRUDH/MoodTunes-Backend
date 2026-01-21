4d9ba0177e13b00cd5b91695cbfcbd8d58b63393
SAIxANIRUDH <chouratsai007@gmail.com>
2026-01-21 09:13:06 +0530

    4th commit

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
 const express = require('express');
-const songroutes = require('./src/routes/song.routes');
-
 const cors = require('cors');
 
+const songRoutes = require('./src/routes/song.routes');
+
 const app = express();
+
 app.use(cors());
 app.use(express.json());
-app.use('/',songroutes);
 
+
+app.get('/', (req, res) => {
+  res.send('Backend is running 🚀');
+});
+
+app.use('/api/songs', songRoutes);
 
 module.exports = app;
